8cfe100125b072182b1e5e478153e8902026a4d7
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-05-30 05:25:21 +0700

    dddd

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from unittest import result
 from selenium import webdriver
 from openpyxl import load_workbook
 import time
+import os
+from tqdm import tqdm
+
 username = "na_as_ttytanhson"
 password = "Jw3@b@qnPjj7vUT"
 
@@ -22,68 +25,62 @@ time.sleep(5)
 otp = input("Nhập OTP: ")
 
 otp_input = driver.find_element_by_id("otp")
+otp_input.clear()
 otp_input.send_keys(otp)
 submit_btn = driver.find_element_by_xpath("/html/body/app-root/app-login/div/div[2]/div/div[3]/di/button")
 submit_btn.click()
 time.sleep(5)
 
-driver.get("http://hssk.kcb.vn/#/ho-so-suc-khoe")
-time.sleep(3)
-advanced_search = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/div[3]/app-base-button[2]/button/span')
-advanced_search.click()
-time.sleep(1)
-name_input = driver.find_element_by_xpath('//*[@id="demo"]/div/div[5]/div/input')
-birth_input = driver.find_element_by_xpath('//*[@id="demo"]/div/div[17]/div/app-datepicker/div/input')
-search_btn = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/div[3]/app-base-button[1]/button')
-
-
-
-dest_filename = 'Thong_ke_xac_minh_doi_tuong.xlsx'
+xa = 'phucson'
+dest_filename = 'Thong_ke_xac_minh_doi_tuong_' + xa + '.xlsx'
+tmp_filename = 'tmp_' + xa + '.xlsx'
+backup_filename = 'backup_' + xa + '.xlsx'
 wb = load_workbook(filename = dest_filename)
 ws = wb.active
-for i in range(9, ws.max_row+1):
-    name = ws.cell(i,4).value #Họ và tên
-    birth = ws.cell(i,5).value #Ngày sinh
+for i in tqdm(range(7, ws.max_row+1)):
+    driver.get("http://hssk.kcb.vn/#/ho-so-suc-khoe")
+    time.sleep(1)
+    try:
+        advanced_search = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/div[3]/app-base-button[2]/button/span')
+        advanced_search.click()
+        name_input = driver.find_element_by_xpath('//*[@id="demo"]/div/div[5]/div/input')
+        birth_input = driver.find_element_by_xpath('//*[@id="demo"]/div/div[17]/div/app-datepicker/div/input')
+        search_btn = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/div[3]/app-base-button[1]/button')
+        name = ws.cell(i,5).value #Họ và tên
+        birth = ws.cell(i,6).value #Ngày sinh
 
-    name_input.clear()
-    name_input.send_keys(name)
-    birth_input.clear()
-    birth_input.send_keys(birth)
-    search_btn.click()
-    print("Đang tìm kiếm: ", name)
-    
-    time.sleep(5)
+        name_input.clear()
+        name_input.send_keys(name)
+        birth_input.clear()
+        birth_input.send_keys(birth)
+        search_btn.click()
+        
+        time.sleep(1)
 
-    result_search = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/span[2]')
-
-    # Nếu tìm thấy kết quả
-    if result_search.text == "(1)":
-        print("Đã tìm thấy kết quả")
-        diachithuongtru_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr/td[9]/div')
-        phone_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr/td[7]/div')
-
-        print(diachithuongtru_output.text)
-        print(phone_output.text)
-
-        ws.cell(i,2).value = diachithuongtru_output.text
-        ws.cell(i,3).value = phone_output.text
-        wb.save(dest_filename)
-        time.sleep(5)
-        print("Cập nhập thành công")
-    elif result_search.text == "(0)":
-        print("Không tìm thấy")
-        ws.cell(i,2).value = "Không tìm thấy"
-        wb.save(dest_filename)
-        time.sleep(5)
-    else:
-        print("Tìm thấy nhiều hơn 1 kết quả")
-        ws.cell(i,2).value = "nhiều hơn 1 kết quả"
-        wb.save(dest_filename)
-        time.sleep(5)
-
-
-
-
+        result_search = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/span[2]')
+        # Nếu tìm thấy kết quả
+        if result_search.text == "(1)":
+            diachithuongtru_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr/td[9]/div')
+            phone_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr/td[7]/div')
+            madinhdanhyte_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr/td[3]/div/a')
+            ws.cell(i,2).value = diachithuongtru_output.text
+            ws.cell(i,3).value = phone_output.text
+            chitiet_btn = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-info-search/search-table/div/div/div[1]/table/tbody/tr[1]/td[11]/div/div[1]/span/button')
+            chitiet_btn.click()
+            time.sleep(1)
+            cccd_output = driver.find_element_by_xpath('//*[@id="app-content"]/div[1]/app-nhan-khau-chi-tiet/div/div[2]/div/div[10]/span[2]')
+            ws.cell(i,4).value = cccd_output.text
+        elif result_search.text == "(0)":
+            ws.cell(i,2).value = "Không tìm thấy"
+        else:
+            ws.cell(i,2).value = "nhiều hơn 1 kết quả"
+        
+        wb.save(tmp_filename)
+        os.rename(dest_filename, backup_filename)
+        os.rename(tmp_filename, dest_filename)
+        os.remove(backup_filename)
+    except:
+        print("Lỗi")
 
 
 driver.quit()
---
+++ suathongtin_congtiemchung.py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from openpyxl import load_workbook
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+from tqdm import tqdm
+
+username = "na_anhson"
+password = "Hieu@TTYT123"
+
+driver = webdriver.Chrome()
+driver.get("https://tiemchungcovid19.moh.gov.vn/")
+time.sleep(5)
+
+username_input = driver.find_element_by_id("username")
+password_input = driver.find_element_by_id("password")
+submit_btn = driver.find_element_by_id("btnLogin")
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+submit_btn.click()
+
+time.sleep(5)
+otp = input("Nhập OTP: ")
+
+otp_input = driver.find_element_by_id("otpTxt")
+otp_input.send_keys(otp)
+submit_btn = driver.find_element_by_xpath("/html/body/div/div/div/div/button[1]")
+submit_btn.click()
+time.sleep(5)
+
+xa = 'camson'
+dest_filename = 'Thong_ke_xac_minh_doi_tuong_' + xa + ' - Copy.xlsx'
+tmp_filename = 'copy_tmp'+ xa + ' - Copy.xlsx'
+backup_filename = 'copy_backup'+ xa + ' - Copy.xlsx'
+wb = load_workbook(filename = dest_filename)
+ws = wb.active
+for i in tqdm(range(7, ws.max_row+1)):
+    try:
+        cccd_key = ws.cell(i, 4)
+        if (cccd_key.value != None) and (cccd_key.value != "Không có thông tin"):
+            driver.get("https://tiemchungcovid19.moh.gov.vn/TiemChung/DoiTuong/IndexCovid")
+            time.sleep(1)
+
+            driver.find_element_by_xpath('/html/body/section/div[2]/div[3]/div[1]/div[1]/div/div[1]/table/tbody/tr/td[2]/button').click()
+            search_name = ws.cell(i,5).value #Ttên
+            search_phone = ws.cell(i, 10).value # SĐT
+            search_name_input = driver.find_element_by_id('txtHoTenSearch')
+            search_phone_input = driver.find_element_by_id('txtSoDienThoaiSearch')
+
+            search_name_input.send_keys(search_name)
+            search_phone_input.send_keys(search_phone)
+        
+
+            search_btn = driver.find_element_by_id('btnAdvancedSearch')
+            clear = driver.find_element_by_xpath('//*[@id="select2-slDonViTao-container"]/span')
+            clear.click()
+            search_btn.click()
+            time.sleep(1)
+            driver.find_element_by_xpath('//*[@id="AdvancedSearchPopover"]/h3/span').click()
+
+            link_edit = driver.find_element_by_xpath('//*[@id="doiTuongSearchResult"]/tbody/tr/td[2]/div')
+            link_edit.click()
+            time.sleep(1)
+
+            btn_edit = driver.find_element_by_id('btnEdit')
+            btn_edit.click()
+            time.sleep(1)
+
+            driver.find_element_by_xpath('//*[@id="thongTinCoBan"]/div[2]/div[2]/span/span[1]/span/span[2]').click()
+            driver.find_element_by_xpath('/html/body/span/span/span[1]/input').send_keys("Việt Nam")
+            driver.find_element_by_xpath('/html/body/span/span/span[1]/input').send_keys(Keys.ENTER)
+
+            cccd_input = driver.find_element_by_id('txtCMT_Sua')
+            cccd_input.clear()
+
+            cccd_input.send_keys(cccd_key.value)
+            save_btn = driver.find_element_by_id('btnSave')
+            save_btn.click()
+            time.sleep(1)
+
+            ws.cell(i,1).value = "Done"
+        else:
+            ws.cell(i,1).value = "Failed"
+        wb.save(tmp_filename)
+        os.rename(dest_filename, backup_filename)
+        os.rename(tmp_filename, dest_filename)
+        os.remove(backup_filename)
+    except:
+        print("Lỗi")
+
+driver.quit()
