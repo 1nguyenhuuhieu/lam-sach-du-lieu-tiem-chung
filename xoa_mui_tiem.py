89a4335c6a6fd9da73dff41a1ec6f46af6202e22
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-05-31 13:55:47 +0700

    ghg

--- xoa_mui_tiem.py
+++ xoa_mui_tiem.py
@@ -8,7 +8,7 @@ username = "na_anhson"
 password = "Hieu@TTYT123"
 
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("https://tiemchungcovid19.moh.gov.vn/")
 time.sleep(2)
 
@@ -20,20 +20,19 @@ username_input.send_keys(username)
 password_input.send_keys(password)
 submit_btn.click()
 
-time.sleep(5)
+time.sleep(2)
 otp = input("Nhập OTP: ")
 
 otp_input = driver.find_element_by_id("otpTxt")
 otp_input.send_keys(otp)
 submit_btn = driver.find_element_by_xpath("/html/body/div/div/div/div/button[1]")
 submit_btn.click()
-time.sleep(5)
+time.sleep(2)
 
-xa = 'binhson' 
-dest_filename = 'xoa_' + xa + '.xlsx'
+dest_filename = 'xoa.xlsx'
 wb = load_workbook(filename = dest_filename)
 ws = wb.active
-for i in tqdm(range(7, ws.max_row+1)):
+for i in tqdm(range(6, ws.max_row+1)):
     try:
         driver.get("https://tiemchungcovid19.moh.gov.vn/TiemChung/DoiTuong/IndexCovid")
         time.sleep(1)
@@ -56,17 +55,25 @@ for i in tqdm(range(7, ws.max_row+1)):
         link_edit.click()
         time.sleep(1)
         driver.find_element_by_xpath('//*[@id="AdvancedSearchPopover"]/h3/span').click()
-
         driver.find_element_by_xpath('//*[@id="tab-user-info"]/li[2]/a').click()
 
-        try:
-            driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[2]/td[3]')
-            try:
-                driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[1]/td[3]')
-            except:
-                driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[2]/td[8]/a[6]').click()
-        except:
-            pass
+        d = driver.execute_script("return document.getElementById('tblVacxin').rows.length;")
+
+        if d==2:
+            driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr/td[8]/a[6]/i').click()
+            driver.find_element_by_xpath('/html/body/div[9]/div/div/div[3]/button[2]').click()
+        
+
+        # try:
+        #     driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[2]/td[3]')
+        #     try:
+        #         driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[1]/td[3]')
+        #     except:
+        #         driver.find_element_by_xpath('//*[@id="tblVacxin"]/tbody/tr[2]/td[8]/a[6]/i').click()
+        #         print("Đã xóa")
+        #         time.sleep(1)
+        # except:
+        #     pass
         time.sleep(1)
 
     except:
